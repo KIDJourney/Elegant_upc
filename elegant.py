ac13462ab11febc97a515b08c50210e45c5558d4
KIDJourney <kingdeadfish@qq.com>
2015-06-20 17:28:37 +0800

    remove

--- elegant.py
+++ elegant.py
@@ -3,20 +3,21 @@
 # @Author: kidjourney
 # @Date:   2015-05-29 11:23:13
 # @Last Modified by:   kidjourney
-# @Last Modified time: 2015-06-16 12:28:07
+# @Last Modified time: 2015-06-17 09:18:29
 
 import requests
 import bs4
+import configparser
 
 class elegantupc :
     def __init__(self):
         self.url_index = "http://cas.upc.edu.cn/cas/login?service=http://i.upc.edu.cn/dcp/index.jsp"
         self.url_login = "http://cas.upc.edu.cn/cas/login"
-        self.user_username = "1307010412"
-        self.user_password = "Andthejourney123"
+        self.url_userpage = "http://i.upc.edu.cn/dcp/forward.action?path=/portal/portal&p=wkHomePage"
+        self.user_username = ""
+        self.user_password = ""
         self.user_token = ""
-        self.user_request = None
-        self.user_index = None
+        self.user_request = None #container of status of connection
 
     def get_token(self):
         try :
@@ -49,12 +50,27 @@ class elegantupc :
         response = self.user_request.post(self.url_login,data=payload)
         soup = bs4.BeautifulSoup(response.text)
         redireturl = soup.find('a').get('href')
-        print(redireturl)
+        # getSession
+        self.user_request.get(redireturl)
 
+        response = self.user_request.get(self.url_userpage) #Login to index of digital upc
+        # get Session already
+        # print(response.text)
 
+    def jwxt_login(self):
+        pass
 
+    def library_login(self):
+        response = self.user_request.get('http://i.upc.edu.cn/dcp/forward.action?path=dcp/apps/sso/jsp/ssoDcpSelf&appid=1186')
+        #Get Session
+
+    def card_login(self):
+        response = self.user_request.get('http://card.upc.edu.cn/Login.aspx')
+        soup = bs4.BeautifulSoup(response.text)
+        redirecturl = soup.find('a').get('href')
 
 if __name__ == "__main__":
     job = elegantupc()
     job.get_token()
     job.user_login()
+    job.card_login()
