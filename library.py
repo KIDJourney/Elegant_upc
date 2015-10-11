765e30b2e9e86456034e5e15d4b65de531320700
KIDJourney <kingdeadfish@qq.com>
2015-10-11 13:52:31 +0800

    add self session worker

--- library.py
+++ library.py
@@ -6,16 +6,16 @@ import authbase
 
 class Library(authbase.AuthBase):
     def __init__(self, session_worker):
-        super(Library,self).__init__(session_worker)
+        super(Library, self).__init__(session_worker)
 
         self.config = common.get_config()
-        self.library_redirect = self.config['url']['library_redirect']
+        self.library_redirect = self.config['url']['library_auth']
+        self.session_worker= session_worker
 
     def access_session(self):
         response = self.session_worker.get(self.library_redirect)
         response.encoding = 'utf8'
         return response
-
 
     def get_books_borrowed(self):
         pass
