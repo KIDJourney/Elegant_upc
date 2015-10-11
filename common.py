f89c5433aab8ce56094957ad3593cf9a2d59cf3e
KIDJourney <kingdeadfish@qq.com>
2015-10-11 13:51:38 +0800

    -move access url to a common method

--- common.py
+++ common.py
@@ -1,6 +1,6 @@
 import requests
 import configparser
-
+import bs4
 
 def login_check(session_worker):
     if not isinstance(session_worker,requests.Session):
@@ -16,4 +16,16 @@ def get_config():
     config = configparser.ConfigParser()
     config.read('config.ini')
     return config
+
+
+def access_url(session_worker, url, method='get', payload=None):
+    active = {
+        'get': session_worker.get(url),
+        'post': session_worker.post(url, data=payload)
+    }
+
+    if method not in ('get', 'post'):
+        raise Exception("Method Error : Method %s does not exist ")
+
+    return bs4.BeautifulSoup(active[method.lower()].text)
 
