15d1d69777d78ef4e9b77d6d9acae052e1cf29ef
KIDJourney <kingdeadfish@qq.com>
2015-10-14 00:05:54 +0800

    manauly get cookie from jwxt

--- stums.py
+++ stums.py
@@ -2,7 +2,7 @@ import requests
 import bs4
 import common
 import authbase
-
+import re
 
 class StudentManageSystem(authbase.AuthBase):
     def __init__(self, session_worker):
@@ -12,13 +12,18 @@ class StudentManageSystem(authbase.AuthBase):
         config = common.get_config()
         self.auth_url = config['url']['stums_auth']
         self.grade_url = config['stums']['grade']
+        self.jwxt_cookie = {}
 
     def access(self):
-        return self.session_worker.get(self.auth_url)
+        response = self.session_worker.get(self.auth_url)
+        self.jwxt_cookie['JSESSIONID'] = re.findall('"[A-Z0-9a-z]+"', response.text)
+        print(self.jwxt_cookie)
+        return response
 
     def get_grade(self, page=1):
         request_payload = common.grade_check_payload_factor(page)
-        return self.session_worker.post(self.grade_url, data=request_payload)
+        response = self.session_worker.post(self.grade_url, data=request_payload)
+        return response
 
     def get_syllabus(self):
         pass
