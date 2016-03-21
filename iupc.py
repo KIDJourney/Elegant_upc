2e385c3a038089b273680719a4e4b04627c7fe55
KIDJourney <kingdeadfish@qq.com>
2016-03-21 14:26:18 +0800

    add response method

--- iupc.py
+++ iupc.py
@@ -20,13 +20,15 @@ class iupc():
 
         self.password = hashlib.md5(self.password.encode()).hexdigest()
 
-    def is_available(self):
-        response = self.get_content(self.url_index)
+    def is_available(self):       
+        response = self.get_response(self.url_index)
+        return response.url == "http://i.upc.edu.cn/dcp/forward.action?path=/portal/portal&p=home"
+        
         # return response.status_code == 200
         # return 'CAS认证转向' in response.text
 
     def get_token(self):
-        soup = self.get_content(self.url_login)
+        soup = self.get_soup(self.url_login)
         token = soup.find('input', {'name': 'lt'}).get('value')
         return token
 
@@ -40,7 +42,7 @@ class iupc():
         if "错误的用户名或密码" in response.text:
             raise Exception("Username or password incorrect")
 
-        access_response = self.get_content(self.url_index)
+        access_response = self.get_soup(self.url_index)
 
         access_url = access_response.find('a').get('href')
 
@@ -51,8 +53,11 @@ class iupc():
 
         # return "错误" not in response.text
 
+    def get_response(self, *args, **kwargs):
+        return self.session.get(*args, **kwargs)
+
     @bs4_decorator
-    def get_content(self, *args , **kwargs):
+    def get_soup(self, *args , **kwargs):
         return self.session.get(*args , **kwargs)
 
 if __name__ == "__main__":
